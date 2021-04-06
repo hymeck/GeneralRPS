3613bc82454dd2056fdb1da57b337e0bd4c24c87
Hymeck <noonimf@gmail.com>
2021-04-07 01:12:53 +0300

    remove unused enum extension

--- src/Core/Result.cs
+++ src/Core/Result.cs
@@ -1,20 +1,9 @@
-﻿using System;
-
-namespace GeneralRPS.Core
+﻿namespace GeneralRPS.Core
 {
     public enum Result
     {
         Lose = -1,
         Draw = 0,
         Win = 1
-    }
-
-    public static class ResultExtensions
-    {
-        public static Result Inverse(this Result result) =>
-            (Result)(-(int) result);
-
-        public static Result ToResult(this int value) =>
-            (Result) Math.Sign(value);
     }
 }
